b8a477bc1668cc19ba716f58332295a22d0f8b07
Ivan Tarasiuk <viix345@gmail.com>
2024-02-11 20:53:04 +0100

    Content page has been done and conent.png added to github

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Bootstrap Bundle JS
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-import { Home, Footer, Landing } from './components';
+import { Home, Footer, Landing, Content } from './components';
 
 function App() {
   return (
     <div className="wholeApp">
       <main>
         <Landing />
+        <Content />
         {/* <article className="second_article">
           <header>2 ARTICLE</header>
           <h2>Leroudfgjfdklgjfdl ldfk jgdf lkjgdflk jgdf </h2>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {Home} from './Home_page/Home.jsx';
 export {Navbar} from './Navbar/Navbar.jsx';
 export {Footer} from './Footer/Footer.jsx';
 export {Landing} from './Landing_page/Landing.jsx';
+export {Content} from './Content_page/Content.jsx';
