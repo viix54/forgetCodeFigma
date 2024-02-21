833c23b3510df120645b930b0863c9806a0227fb
Ivan Tarasiuk <viix345@gmail.com>
2024-02-21 23:15:11 +0300

    Testimonial Page has been done till the end

--- src/components/Testimonial_page/Testimonial.jsx
+++ src/components/Testimonial_page/Testimonial.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import './testimonial.scss';
 
-import client1 from '../../assets/happy_clients/client1.jpg';
+import { clients } from './clientsData';
+
+const newClientOpinion = (client, index) => {
+  return (
+    <section key={index + 'person'}>
+      <div className="person">
+        {/* <div className="picture"></div> */}
+        <img src={`../../src/assets/happy_clients/${client.photo}`} className="picture" alt="" />
+        <div className="section_text">
+          <p>{client.opinion}</p>
+          <span>{client.firstName + ' ' + client.secondName}</span>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export const Testimonial = () => {
   return (
@@ -9,17 +24,17 @@ export const Testimonial = () => {
       <article className="testimonial_article">
         <header>Our Happy Clients</header>
         <div className="happy_clients_sections">
-          <section>
-            <div className="picture"></div>
-            <div className="section_text">
-              <p>
-                Get a fully retina ready site when you build with Startup Framework. Websites look
-                sharper and more gorgeous on devices with retina display support
-              </p>
-              <span>Rayhan Curran</span>
-            </div>
-            {/* <img src={client1} alt="client" /> */}
-          </section>
+          {clients.map((client, index) => newClientOpinion(client, index))}
+          {/* <div className="person">
+              <div className="picture"></div>
+              <div className="section_text">
+                <p>
+                  Get a fully retina ready site when you build with Startup Framework. Websites look
+                  sharper and more gorgeous on devices with retina display support
+                </p>
+                <span>Rayhan Curran</span>
+              </div>
+            </div> */}
         </div>
       </article>
     </div>
---
+++ src/components/Testimonial_page/clientsData.js
@@ -0,0 +1,26 @@
+export const clients = [
+    {
+        firstName:'Rayhan',
+        secondName:'Curran',
+        opinion:'Get a fully retina ready site when you build with Startu   Framework. Websites look sharper and more gorgeous on devices with retin   display support',
+        photo:'client1.jpeg',
+    },
+    {
+        firstName:'Kayley',
+        secondName:'Frame',
+        opinion:'As a business targeting high net worth individuals, we were looking for a slick, cool and mini-malistic design for our website',
+        photo:'client2.jpg',
+    },
+    {
+        firstName:'Gene',
+        secondName:'Whitfield',
+        opinion:'The most important part of the Startup Framework is the samples',
+        photo:'client3.jpeg',
+    },
+    {
+        firstName:'Allan',
+        secondName:'Kim',
+        opinion:'I`ve built my website with Startup just in one day, and it was ready-to-go. ',
+        photo:'client4.jpg',
+    }
+]
