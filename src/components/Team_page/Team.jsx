fa6b6386dd84ea871d4491a8f2eccd88476132c9
Ivan Tarasiuk <viix345@gmail.com>
2024-02-23 00:28:17 +0300

    Team page has been done

--- src/components/Team_page/Team.jsx
+++ src/components/Team_page/Team.jsx
@@ -2,16 +2,37 @@ import React from 'react';
 
 import './team.scss';
 import { crew_data } from './crew_data';
+import {
+  FaInstagram,
+  FaGit,
+  FaTwitter,
+  FaMediumM,
+  FaFacebook,
+  FaDribbble,
+  FaSkype,
+} from 'react-icons/fa';
+
+const socialNetTags = {
+  instagram: <FaInstagram />,
+  git: <FaGit />,
+  twitter: <FaTwitter />,
+  medium: <FaMediumM />,
+  facebook: <FaFacebook />,
+  dribbble: <FaDribbble />,
+  skype: <FaSkype />,
+};
 
 const crewMember = (member, index) => {
   return (
-    <section key={member + index}>
+    <section key={member.firstName + '_' + member.secondName + '_' + index}>
       <img src={`../../src/assets/crew_members/${member.photo}`} alt={`Crew member ${index + 1}`} />
       <h1>{member.firstName + ' ' + member.secondName}</h1>
       <span>{member.work_position}</span>
       <div className="linksToMember">
         {member.links.map((link) => (
-          <div key={member.firstName + ' ' + member.secondName + ' ' + link}>{link}</div>
+          <div key={member.firstName + ' ' + member.secondName + ' ' + link}>
+            {socialNetTags[link]}
+          </div>
         ))}
       </div>
     </section>
@@ -29,9 +50,9 @@ export const Team = () => {
             of 25 usable pages you can use as is or you can add new blocks from UI Kit.
           </p>
         </header>
-        <section className="crew_member_container">
+        <div className="crew_member_container">
           {crew_data.map((member, index) => crewMember(member, index))}
-        </section>
+        </div>
       </article>
     </div>
   );
