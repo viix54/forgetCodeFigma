acbd4b10b345a2fbe6db111a00376e3bde7b08f3
Ivan Tarasiuk <viix345@gmail.com>
2024-03-02 01:30:36 +0300

    Found a problem with drid, solved it

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './footer.css';
+import './footer.scss';
 
 export const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return <footer className="footer">Footer</footer>;
 };
